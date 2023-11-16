f1eaaa62d243565ffe417dc14c6cae1ae768b0f5
HardcatAntoine <br41nk1ck3r@gmail.com>
2023-11-16 14:25:46 +0300

    add WeatherFragment, WeatherViewModel

---
+++ app/src/main/java/com/example/weatherapptest/view/WeatherFragment.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapptest.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weatherapptest.R
+
+class WeatherFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/weatherapptest/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.weatherapptest.viewmodel
+
+class WeatherViewModel {
+}
