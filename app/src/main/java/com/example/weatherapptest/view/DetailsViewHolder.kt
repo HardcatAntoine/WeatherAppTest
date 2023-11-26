867c17ffd9e8118580a5eac696af329cd8a63e04
HardcatAntoine <br41nk1ck3r@gmail.com>
2023-11-26 18:02:53 +0300

    refactor layouts

--- app/src/main/java/com/example/weatherapptest/view/DetailsViewHolder.kt
+++ app/src/main/java/com/example/weatherapptest/view/DetailsViewHolder.kt
@@ -9,7 +9,6 @@ import com.example.weatherapptest.data.model.DataList
 class DetailsViewHolder(itemView: View) : ViewHolder(itemView) {
     private val date = itemView.findViewById<TextView>(R.id.date_time_details)
     private val tempMax = itemView.findViewById<TextView>(R.id.temp_max_details)
-    private val tempMin = itemView.findViewById<TextView>(R.id.temp_min_details)
     private val pressure = itemView.findViewById<TextView>(R.id.pressure_details)
     private val humidity = itemView.findViewById<TextView>(R.id.humidity_details)
     private val windSpeed = itemView.findViewById<TextView>(R.id.wind_speed_details)
@@ -17,12 +16,11 @@ class DetailsViewHolder(itemView: View) : ViewHolder(itemView) {
 
     fun bindDetails(item: DataList) {
         date.text = item.dt_txt
-        tempMin.text = item.main.temp_min.toString()
-        tempMax.text = item.main.temp_max.toString()
-        pressure.text = item.main.pressure.toString()
-        humidity.text = item.main.humidity.toString()
-        windSpeed.text = item.wind.speed.toString()
-        clouds.text = item.clouds.all.toString()
+        tempMax.text = "${item.main.temp_max}°C"
+        pressure.text = "${item.main.pressure} hPa"
+        humidity.text = "${item.main.humidity}%"
+        windSpeed.text = "${item.wind.speed} m/sec"
+        clouds.text = "${item.clouds.all}%"
 
     }
 }
